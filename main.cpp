aea68b5d8c0901640671080a9e6d5706ebfc76c0
Arthur Brugiere <arbr1@hotmail.fr>
2016-11-15 19:38:26 +0100

    Création 1 Eleve (nom)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <fstream>
 
-int main(int argc, char *argv[])
+#include "Eleve.h"
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    std::string eleveName;
+    Eleve student1;
+    char ch;
+    std::fstream fin("data/students.txt", std::fstream::in);
+    while (fin >> std::noskipws >> ch) {
+        std::cout << ch; // Or whatever
+        if (ch != '['){
+            eleveName += ch;
+        }else{
+            student1.setName(eleveName);
+        }
 
-    return a.exec();
+        if(ch == '\n'){
+            break;
+        }
+    }
+
+	std::cout << "Hello World" << std::endl;
+	std::cout << student1.getName();
+	return 0;
 }
+
